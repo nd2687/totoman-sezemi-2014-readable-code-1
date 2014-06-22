dd9f2a39d7ec27ceca41b960130bb021c7101ad9
Takashi Kawano <maketakunaiman@gmail.com>
2014-06-22 11:24:01 +0900

    spec6: レシピID指定

--- siyou.rb
+++ siyou.rb
@@ -1,5 +1,11 @@
 recipe = File.open("recipe-data.txt")
 
+id = ARGV[0]
+
 recipe.each_with_index do |v, i|
-  puts "#{i+1}: #{v}"
+  if id.to_i == i+1
+    puts "#{i+1}: #{v}"
+  elsif id == nil
+    puts "#{i+1}: #{v}"
+  end
 end
